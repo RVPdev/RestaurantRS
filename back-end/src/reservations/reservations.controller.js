25d881d10125c396d91684f6d046a11013eb02be
RenatoDevi <renatovilcahuaman@gmail.com>
2023-09-27 15:07:51 -0500

    knex table and migrations

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -4,6 +4,7 @@
 const service = require("./reservations.service");
 
 async function list(req, res, next) {
+  console.log("11111111111111111")
   service
     .list()
     .then((data) => res.json({ data }))
