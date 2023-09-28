aba60c0b0d43d318542666ca1a9e0c77e11cf867
RenatoDevi <renatovilcahuaman@gmail.com>
2023-09-28 11:30:13 -0500

    added errors

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -6,12 +6,20 @@
 
 const router = require("express").Router();
 const controller = require("./reservations.controller");
-
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // reservation id
-router.route("/:reservationId").get(controller.read)
+router
+  .route("/:reservationId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
+  .all(methodNotAllowed);
 
-router.route("/").get(controller.list);
-
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
